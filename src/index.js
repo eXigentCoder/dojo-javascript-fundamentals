2c00603066e45a45f729b873cf26edd6c06bdc43
Ryan Kotzen <potz666@gmail.com>
2018-02-10 17:19:21 +0200

    More slides and memes

--- src/index.js
+++ src/index.js
@@ -1,36 +1,12 @@
 'use strict'
-
-// let a = 0363 //error
-// let a = 0o363; //fine
-//
-
-// Duplicate properties
-
-//
-// , not in ES6 see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#Duplicate_property_names
-//
-// // Assignment to a non-writable property
-// let obj1 = {}
-// Object.defineProperty(obj1, 'x', { value: 42, writable: false })
-// obj1.x = 9 // throws a TypeError
-//
-// // Assignment to a getter-only property
-// let obj2 = {
-//     get x() {
-//         return 17
-//     }
-// }
-// obj2.x = 5 // throws a TypeError
-//
-// // Assignment to a new property on a non-extensible object
-// let fixed = {}
-// Object.preventExtensions(fixed)
-// fixed.newProp = 'ohai' // throws a TypeError
-
-// Throws an error if you try delete undeletable properties. previously no effect.
-//delete Object.prototype // throws a TypeError
-
-// Throws an error when trying to set a property on a primitive
-//(1).hiThere = "heya!"
-
-and more https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+let myObject = {}
+myObject.newProp = {}
+myObject.newProp.isAwesome = true
+const propName = 'Awesome Property';
+myObject[propName] = {
+    a:1,
+    b:2,
+    c:3
+}
+console.log(myObject.newProp.isAwesome) // true
+console.log(Object.keys(myObject[propName]).length) //3
