6270265a139cc1b5a70f22102859db6efed05c06
Ryan Kotzen <potz666@gmail.com>
2018-02-10 20:40:31 +0200

    fml this is taking forever

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 'use strict'
-let myObject = {}
-myObject.newProp = {}
-myObject.newProp.isAwesome = true
-const propName = 'Awesome Property';
-myObject[propName] = {
-    a:1,
-    b:2,
-    c:3
-}
-console.log(myObject.newProp.isAwesome) // true
-console.log(Object.keys(myObject[propName]).length) //3
+console.log(typeof ('' + 5)) //string
+console.log(typeof ('1' + 5)) //string
+console.log(typeof (5 + '1')) //string
+console.log(typeof ('1' - 5)) //number
+console.log(typeof ('1' * 5)) //number
+console.log('1' < 5) //true
+console.log('1' < '5') //true NOTE this is alphabetically less.
+console.log('b' < 'a') //false (alphabetically)
+console.log('4' == 4) //true
