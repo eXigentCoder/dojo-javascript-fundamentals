0afc8f9badc68dd6b55bcccee012e0cb03b36093
Ryan Kotzen <potz666@gmail.com>
2018-02-12 08:59:09 +0200

    Demo time soon!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 'use strict'
 
-// this:
-const x = new Error('I was created using a function call!')
-// has the same functionality as this:
-const y = new Error('I was constructed via the "new" keyword!')
+function Car(make = 'Honda', model = 'Accord', year = 1998) {
+    this.make = make
+    this.model = model
+    this.year = year
+    this.drive = function() {
+        console.log(`Starting drive of a ${this.year} ${this.make} ${this.model}`)
+        // Starting drive of a 1967 Chevy Impala
+        setTimeout(function() {
+            console.log(`Ending drive of a ${this.year} ${this.make} ${this.model}`)
+            // Ending drive of a undefined undefined undefined
+        }, 500)
+    }
+}
 
-throw x
+const demonHunter = new Car('Chevy', 'Impala', 1967)
+demonHunter.drive()
